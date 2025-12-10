2e3eb9a12550f580dc4a951fbe58d03cfece9596
Jarvis Walker <jarvisjw12@gmail.com>
2025-12-10 10:33:12 -0800

    Refactor PantryDB to use value semantics for items

--- PantryDB.h
+++ PantryDB.h
@@ -1,71 +1,88 @@
 #ifndef PANTRYDB_H
 #define PANTRYDB_H
 
+#include "PantryItem.h"
 #include <vector>
 #include <fstream>
-#include <sstream>
-#include <stdexcept>
-#include "PerishableItem.h"
-#include "NonPerishableItem.h"
-using namespace std;   // Added
+#include <iostream>
+using namespace std;
 
 class PantryDB {
+    vector<PantryItem> items;
     string filename;
-    vector<PantryItem*> items;
 
 public:
-    PantryDB(const string& fname) : filename(fname) {}
-    ~PantryDB() {
-        for (auto i : items) delete i;
+    PantryDB(string fname) : filename(fname) { loadFromFile(); }
+
+    void addItem(const PantryItem& item) {
+        items.push_back(item);
+        saveToFile();
     }
 
-    void addItem(PantryItem* item) {
-        items.push_back(item);
+    void listItems() const {
+        for (const auto& item : items) {
+            cout << "Name: " << item.name
+                << " | Qty: " << item.quantity
+                << " | Exp: " << item.expiry << "\n";
+        }
+    }
+
+    void searchItem(const string& searchName) const {
+        for (const auto& item : items) {
+            if (item.name == searchName) {
+                cout << "Found -> " << item.name
+                    << " | Qty: " << item.quantity
+                    << " | Exp: " << item.expiry << "\n";
+                return;
+            }
+        }
+        cout << "Item not found.\n";
+    }
+
+    void deleteItem(const string& deleteName) {
+        for (auto it = items.begin(); it != items.end(); ++it) {
+            if (it->name == deleteName) {
+                items.erase(it);
+                saveToFile();
+                cout << "Item deleted.\n";
+                return;
+            }
+        }
+        cout << "Item not found.\n";
+    }
+    void updateQuantity(const string& itemName, int newQty) {
+        for (auto& item : items) {
+            if (item.name == itemName) {
+                item.quantity = newQty;
+                saveToFile();
+                cout << "Quantity updated for " << itemName << ".\n";
+                return;
+            }
+        }
+        cout << "Item not found.\n";
     }
 
     void saveToFile() {
         ofstream ofs(filename);
-        if (!ofs) throw runtime_error("Error: Cannot open file for writing.");
-        for (auto i : items) {
-            ofs << i->serialize() << "\n";
+        for (const auto& item : items) {
+            ofs << item.name << "," << item.quantity << "," << item.expiry << "\n";
         }
     }
 
     void loadFromFile() {
         ifstream ifs(filename);
-        if (!ifs) throw runtime_error("Error: Cannot open file for reading.");
-
         string line;
         while (getline(ifs, line)) {
-            istringstream iss(line);
-            string type;
-            getline(iss, type, ',');
-
-            if (type == "P") {
-                int id, qty; string name, exp, token;
-                getline(iss, token, ','); id = stoi(token);
-                getline(iss, name, ',');
-                getline(iss, token, ','); qty = stoi(token);
-                getline(iss, exp, ',');
-                items.push_back(new PerishableItem(id, name, qty, exp));
-            } else if (type == "N") {
-                int id, qty; string name, token;
-                getline(iss, token, ','); id = stoi(token);
-                getline(iss, name, ',');
-                getline(iss, token, ','); qty = stoi(token);
-                items.push_back(new NonPerishableItem(id, name, qty));
-            } else {
-                throw runtime_error("Error: Unknown item type in file.");
-            }
-        }
-    }
-
-    void displayAll() const {
-        for (auto i : items) {
-            i->display();  // polymorphic call
+            if (line.empty()) continue;
+            PantryItem item;
+            size_t p1 = line.find(',');
+            size_t p2 = line.find(',', p1 + 1);
+            item.name = line.substr(0, p1);
+            item.quantity = stoi(line.substr(p1 + 1, p2 - p1 - 1));
+            item.expiry = line.substr(p2 + 1);
+            items.push_back(item);
         }
     }
 };
 
 #endif
-
