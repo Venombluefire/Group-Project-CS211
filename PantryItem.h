b3b3a2483030418597e5ce311b8d3073f234d20c
Jarvis Walker <jarvisjw12@gmail.com>
2025-12-10 10:33:58 -0800

    Refactor PantryItem from class to struct
    Changed class PantryItem to struct and added expiry field.

--- PantryItem.h
+++ PantryItem.h
@@ -2,26 +2,12 @@
 #define PANTRYITEM_H
 
 #include <string>
-using namespace std;   // Added
+using namespace std;
 
-class PantryItem {
-protected:
-    int id;
+struct PantryItem {
     string name;
     int quantity;
-
-public:
-    PantryItem(int id, const string& name, int qty)
-        : id(id), name(name), quantity(qty) {}
-    virtual ~PantryItem() = default;
-
-    virtual string serialize() const = 0;   // pure virtual
-    virtual void display() const = 0;       // polymorphic display
-
-    int getId() const { return id; }
-    string getName() const { return name; }
-    int getQuantity() const { return quantity; }
+    string expiry;
 };
 
 #endif
-
